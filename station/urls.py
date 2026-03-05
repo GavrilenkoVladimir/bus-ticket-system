949107c40a58c90f4ea133f09a940dbfb11de12e
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-03-05 11:36:22 +0200

    feat:Implement different views

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,37 @@
 from django.urls import path
 
-from station.views import bus_list, bus_detail
+from station.views import (
+    BusList,
+    BusDetail,
+    BusListGM,
+    BusDetailGM,
+    BusListGV,
+    BusDetailGV,
+    BusViewSet
+)
 
 app_name = "station"
 
+bus_list = BusViewSet.as_view({
+    "get": "list",
+    "post": "create"
+})
+bus_detail = BusViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
+
 urlpatterns = [
-    path("buses/", bus_list, name="bus_list"),
-    path("buses/<int:pk>", bus_detail, name="bus_detail"),
+    path("FBV/buses/", bus_list, name="bus_list"),
+    path("FBV/buses/<int:pk>", bus_detail, name="bus_detail"),
+    path("CBV/APIV/buses/", BusList.as_view(), name="bus_list_apiview"),
+    path("CBV/APIV/buses/<int:pk>/", BusDetail.as_view(), name="bus_detail_apiview"),
+    path("CBV/GMAPIV/buses/", BusListGM.as_view(), name="bus_list_g_apiview"),
+    path("CBV/GMAPIV/buses/<int:pk>/", BusDetailGM.as_view(), name="bus_detail_g_apiview"),
+    path("CBV/GV/buses/", BusListGV.as_view(), name="bus_list_g_view"),
+    path("CBV/GV/buses/<int:pk>/", BusDetailGV.as_view(), name="bus_detail_g_view"),
+    path("CBV/GVS/buses/", bus_list, name="bus_list_g_view_set"),
+    path("CBV/GVS/buses/<int:pk>/", bus_detail, name="bus_detail_g_view_set"),
 ]
--- station/views.py
+++ station/views.py
@@ -1,36 +1,47 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework import status, generics, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.mixins import (
+    RetrieveModelMixin,
+    ListModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
+'''
+'''
 
-@api_view(["GET", "PUT"])
+
+# Function base views. You can get access to this features form url path before FBV/
+@api_view(["GET", "POST"])
 def bus_list(request):
     if request.method == "GET":
         buses = Bus.objects.all()
         serializer = BusSerializer(buses, many=True)
-        return Response(serializer.data,  status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = BusSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-@api_view(["GET", "PUT", "DELETE"])
-def bus_detail(request, pk:int):
+@api_view(["GET", "POST", "DELETE"])
+def bus_detail(request, pk: int):
     bus = get_object_or_404(Bus, pk=pk)
     if request.method == "GET":
         serializer = BusSerializer(bus)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "PUT":
+    elif request.method == "POST":
         serializer = BusSerializer(bus, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -38,3 +49,108 @@ def bus_detail(request, pk:int):
         bus.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+# Class base views with APIView class. You can get access this features form url path before CBV/APIView/
+class BusList(APIView):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+    def get(self, request) -> Response:
+        buses = Bus.objects.all()
+        serializer = BusSerializer(buses, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request) -> Response:
+        serializer = BusSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BusDetail(APIView):
+
+    def get_object(self, pk) -> Bus:
+        return get_object_or_404(Bus, pk=pk)
+
+    def get(self, request, pk) -> Response:
+        serializer = BusSerializer(self.get_object(pk=pk))
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk) -> Response:
+        serializer = BusSerializer(self.get_object(pk=pk), data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk) -> Response:
+        serializer = BusSerializer(self.get_object(pk=pk), data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk) -> Response:
+        self.get_object(pk).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Class base views with GenericAPIView class. You can get access this
+# features form url path before CBV/GMAPIV/
+class BusListGM(
+    generics.GenericAPIView,
+    ListModelMixin,
+    CreateModelMixin
+):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs) -> Response:
+        return self.create(request, *args, **kwargs)
+
+
+class BusDetailGM(
+    generics.GenericAPIView,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs) -> Response:
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs) -> Response:
+        return self.destroy(request, *args, **kwargs)
+
+
+# Class base views with GenericView class. You can get access this
+# features form url path before CBV/GV/
+class BusListGV(generics.ListAPIView):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+
+class BusDetailGV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+
+# Class base views with GenericViewSet class. You can get access this
+# features form url path before CBV/GVS/
+class BusViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer

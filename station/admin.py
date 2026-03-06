9adda1001713226cd10a5ff65ba862b04e4e7ed4
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-03-06 12:52:34 +0200

    Added some models and migrate them register them in admin

--- station/admin.py
+++ station/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from station.models import Bus
+from station.models import Bus, Order, Facility, Ticket, Trip
 
 admin.site.register(Bus)
+admin.site.register(Order)
+admin.site.register(Facility)
+admin.site.register(Ticket)
+admin.site.register(Trip)
+

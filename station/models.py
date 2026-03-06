4d2a16fa941f547300201b19b971cb8e0d62efa3
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-03-06 09:08:05 +0200

    Added ModelSerializer and property

--- station/models.py
+++ station/models.py
@@ -4,6 +4,13 @@ class Bus(models.Model):
     info = models.CharField(max_length=255, null=True)
     num_seats = models.IntegerField()
 
+    @property
+    def is_small(self):
+        return self.num_seats <= 25
+
+
+
+
     class Meta:
         verbose_name_plural = "buses"
 
--- station/serializers.py
+++ station/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
 from station.models import Bus
 
+'''
+Representing a serializer using a model Serializer.
+'''
+# class BusSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     info = serializers.CharField(max_length=255, required=False)
+#     num_seats = serializers.IntegerField(required=True)
+#
+#     def create(self, validated_data):
+#         return Bus.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.info = validated_data.get("info", instance.info)
+#         instance.num_seats = validated_data.get("num_seats", instance.num_seats)
+#         instance.save()
+#         return instance
 
-class BusSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    info = serializers.CharField(max_length=255, required=False)
-    num_seats = serializers.IntegerField(required=True)
 
-    def create(self, validated_data):
-        return Bus.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.info = validated_data.get("info", instance.info)
-        instance.num_seats = validated_data.get("num_seats", instance.num_seats)
-        instance.save()
-        return instance
+class BusSerializer(serializers.ModelSerializer):
+    is_small = serializers.BooleanField()
+    class Meta:
+        model = Bus
+        fields = "__all__"
 
 
 
